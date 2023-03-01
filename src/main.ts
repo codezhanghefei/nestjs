84db4d54bc9b8728595bb6d9b8c489b9c8c7d0bb
zhanghefei <zhanghefei@kuaishou.com>
2023-03-01 18:13:36 +0800

    init 0.5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+// import { ConfigService } from 'nestjs-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const config: ConfigService = app.get(ConfigService);
+
   await app.listen(3000);
+  Logger.log(`app is listening on port = ${3000} and env is`);
 }
 bootstrap();
